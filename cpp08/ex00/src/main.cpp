044beb6453a71d59ce7a830c0d2756f169ec60d6
Luciana Pereira <rm92014@fiap.com.br>
2024-03-07 23:23:44 -0300

    Update main.cpp

--- cpp08/ex00/src/main.cpp
+++ cpp08/ex00/src/main.cpp
@@ -5,7 +5,31 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/04 20:34:12 by lucperei          #+#    #+#             */
-/*   Updated: 2024/03/04 20:57:47 by lucperei         ###   ########.fr       */
+/*   Created: 2024/03/05 04:01:09 by lucperei          #+#    #+#             */
+/*   Updated: 2024/03/05 04:11:04 by lucperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../include/easyfind.hpp"
+
+int	main(void) {
+	try {
+		std::vector<int> myVector;
+
+		for (int i = 0; i < 5; i++) {
+			myVector.push_back(i);
+		}
+
+		int valueToFind = 3;
+		std::vector<int>::iterator result = easyfind(myVector, valueToFind);
+		std::cout << "Value " << valueToFind << " found at position " << std::distance(myVector.begin(), result) << std::endl;
+
+		valueToFind = 8;
+		result = easyfind(myVector, valueToFind);
+		std::cout << "Value " << valueToFind << " found at position " << std::distance(myVector.begin(), result) << std::endl;
+	} catch (const std::exception& e) {
+		std::cerr << "Exception: " << e.what() << std::endl;
+	}
+
+	return (0);
+}
