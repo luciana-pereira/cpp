4d5a189874eee38033fe1de98540d473b8159ff1
Luciana Pereira <rm92014@fiap.com.br>
2024-04-13 14:57:31 -0300

    Update main.cpp

--- cpp00/ex01/src/main.cpp
+++ cpp00/ex01/src/main.cpp
@@ -17,20 +17,20 @@ int main(void)
 	PhoneBook	phonebook;
 	std::string	command;
 
-	while (1)
+	while (1) // Loop infinito, ou seja, enquanto for verdadeiro ou não entra na condição de saída para ser interrompido.
 	{
 		std::cout << "Enter command (ADD, SEARCH, EXIT): " << std::endl;
-		std::getline(std::cin, command);
-		if(command == "EXIT" || std::cin.eof())
+		std::getline(std::cin, command); // Lê a entrada do usuário com o std::cin e std::getline lê uma linha inteira de entrada e armazena em command.
+		if(command == "EXIT" || std::cin.eof()) // Se o comando for "EXIT" ou se for o final do arquivo (std::cin.eof()) exibe a mensagem abaixo.
 		{
 			std::cout << "Exiting the program. Contacts will be lost forever!" << std::endl;
 			break ;
 		}
 		else if (command == "ADD")
-			phonebook.addContact();
+			phonebook.addContact(); // O método addContact() do objeto phonebook é chamado para adicionar um novo contato.
 		else if (command == "SEARCH")
-			phonebook.searchContact();
-		else
+			phonebook.searchContact(); // O método searchContact() do objeto phonebook é chamado para pesquisar um contato existente.
+		else // Se o comando de entrada não for reconhecido, o programa exibe uma mensagem de erro.
 			std::cout << "Invalid command. Please try again." << std::endl;
 	}
 	return (0);
