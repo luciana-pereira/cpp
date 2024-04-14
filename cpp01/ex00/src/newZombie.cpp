4496ea347c2c95e1cba7afb01f5d4ee965aa3d75
Luciana Pereira <rm92014@fiap.com.br>
2024-04-14 00:13:07 -0300

    Update newZombie.cpp

--- cpp01/ex00/src/newZombie.cpp
+++ cpp01/ex00/src/newZombie.cpp
@@ -1,6 +1,13 @@
 #include "../include/Zombie.hpp"
 
+// A função chamada newZombie que cria dinamicamente um novo objeto da classe Zombie e 
+// retorna um ponteiro para esse objeto. Resumindo, a função newZombie é responsável por 
+// criar um novo objeto da classe Zombie com o nome especificado e retornar um ponteiro para esse objeto.
+
 Zombie	*newZombie(std::string name)
 {
+	// Esta parte da função cria dinamicamente um novo objeto da classe Zombie, utilizando o 
+	// operador new, e passa o parâmetro name para o construtor da classe Zombie. Em seguida, 
+	// retorna o ponteiro para esse novo objeto criado.
 	return (new Zombie(name));
 }
