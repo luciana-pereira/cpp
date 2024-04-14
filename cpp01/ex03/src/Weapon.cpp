45b253d7c4a39d9442c95ffb2f1678f6732390af
Luciana Pereira <rm92014@fiap.com.br>
2024-04-14 14:16:21 -0300

    Update Weapon.cpp

--- cpp01/ex03/src/Weapon.cpp
+++ cpp01/ex03/src/Weapon.cpp
@@ -1,5 +1,14 @@
 #include "Weapon.hpp"
 
+/*
+Defini uma classe Weapon que representa uma arma com um tipo associado. Ele permite a inicialização 
+da arma com um tipo específico, a recuperação desse tipo e a modificação do tipo posteriormente.
+*/
+
+/*
+No construtor uma arma é inicializada com um tipo fornecido como parâmetro. 
+Isso é feito através da inicialização do membro type com o valor passado.
+*/
 Weapon::Weapon(const std::string& initialType) : type(initialType) {}
 
 const std::string& Weapon::getType(void)
