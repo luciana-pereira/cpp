6343969c2e442a0431d46619c047ef566197aee3
Luciana Pereira <rm92014@fiap.com.br>
2024-04-14 00:22:33 -0300

    Update randomChump.cpp

--- cpp01/ex00/src/randomChump.cpp
+++ cpp01/ex00/src/randomChump.cpp
@@ -1,7 +1,17 @@
 #include "../include/Zombie.hpp"
 
+// A função chamada randomChump que cria um objeto Zombie localmente na pilha de execução, 
+// inicializado com o nome fornecido como parâmetro, e então chama o método announce() 
+// desse objeto para que o zombie anuncie sua presença.
+
+
 void	randomChump(std::string name)
 {
+	// Aqui, um objeto da classe Zombie chamado zombie é criado na pilha de execução, 
+	// utilizando o construtor da classe Zombie que recebe um nome como parâmetro. 
+	// Este objeto é criado localmente.
 	Zombie zombie(name);
+	// O método announce() do objeto zombie é chamado, fazendo com que o zombie 
+	// anuncie sua presença imprimindo uma mensagem na saída padrão.
 	zombie.announce();
 }
