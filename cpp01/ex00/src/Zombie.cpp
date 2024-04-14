1157fe41f8e7fd340de104d9cf0fee9ff4ecc652
Luciana Pereira <rm92014@fiap.com.br>
2024-04-14 00:04:21 -0300

    Update Zombie.cpp

--- cpp01/ex00/src/Zombie.cpp
+++ cpp01/ex00/src/Zombie.cpp
@@ -1,12 +1,20 @@
 #include "../include/Zombie.hpp"
 
+// De forma geral está implementação dos métodos da classe Zombie, incluindo o construtor, o destruidor 
+// e o método announce, que são responsáveis por criar, destruir e anunciar um zumbi, respectivamente.
+
+// Define o construtor da classe Zombie. O construtor recebe um parâmetro name do tipo std::string e 
+// inicializa o atributo _name com o valor fornecido. 
 Zombie::Zombie(std::string name) : _name(name) {}
 
+//  Define o destruidor da classe Zombie. No corpo do destruidor, imprime uma mensagem indicando que o 
+// zumbi foi destruído, mostrando o nome do zumbi _name, conforme subject.
 Zombie::~Zombie(void)
 {
 	std::cout << "Zombie " << _name << " has been destroyed!" << std::endl;
 }
 
+// Define o método announce() da classe Zombie, que não recebe parâmetros e não retorna valor so imprime a mensagem.
 void	Zombie::announce(void)
 {
 	std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
