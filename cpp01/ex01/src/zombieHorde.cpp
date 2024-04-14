9d3bd006f5d93c8e2a46b136e3f1f0aab8a49aae
Luciana Pereira <rm92014@fiap.com.br>
2024-04-14 01:03:40 -0300

    Update zombieHorde.cpp

--- cpp01/ex01/src/zombieHorde.cpp
+++ cpp01/ex01/src/zombieHorde.cpp
@@ -1,13 +1,22 @@
 #include "../include/Zombie.hpp"
 
+/*
+Parâmetros da Função:
+N: Representa o número de zombies.
+name: Representa o nome dos zombies.
+*/
 Zombie*	zombieHorde(int N, std::string name)
 {
+	// Declara um ponteiro
 	Zombie* zombieArray;
-
-	zombieArray = new Zombie[N];
-	for (int i = 0; i < N; ++i)
+	// Aloca dinamicamente memória para um array de N objetos Zombie. 
+	// Cada objeto é inicializado com o construtor padrão.
+	zombieArray = new Zombie[N]; 
+	for (int i = 0; i < N; ++i) // Itera sobre o array de zombies.
 	{
+		// Para cada elemento do array, cria um novo objeto Zombie com o nome fornecido 
+		// e o armazena no array.
 		zombieArray[i] = Zombie(name);
 	}
-	return (zombieArray);
+	return (zombieArray); // Retorna o ponteiro para o array de zombies criado dinamicamente.
 }
