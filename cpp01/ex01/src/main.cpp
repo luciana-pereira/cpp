3019d69a9f8aebf3fb8647d8a7f74d7816eb8e07
Luciana Pereira <rm92014@fiap.com.br>
2024-04-14 01:12:12 -0300

    Update main.cpp

--- cpp01/ex01/src/main.cpp
+++ cpp01/ex01/src/main.cpp
@@ -2,13 +2,19 @@
 
 int	main(void)
 {
+	// Declara um ponteiro para objetos Zombie
 	Zombie	*zombieArray;
 
+	// Chama a função zombieHorde para criar um exército de 5 zombies com o nome "BrainEater". 
+	// O ponteiro retornado pela função zombieHorde é armazenado na variável zombieArray.
 	zombieArray = zombieHorde(5, "BrainEater");
-	for (int i = 0; i < 5; ++i)
+	for (int i = 0; i < 5; ++i) // Itera sobre o array de zombies.
 	{
+		// Para cada zombie no array, chama o método announce para anunciar o zombie.
 		zombieArray[i].announce();
 	}
+	// Libera a memória alocada dinamicamente para o array de zombies, garantindo que não 
+	// haja vazamento de memória.
 	delete[] zombieArray;
 	return (0);
 }
