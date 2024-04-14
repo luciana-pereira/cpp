0f011332cb39d504b74b32cca00d43dbc0c6f288
Luciana Pereira <rm92014@fiap.com.br>
2024-04-14 16:18:44 -0300

    Update HumanA.hpp

--- cpp01/ex03/include/HumanA.hpp
+++ cpp01/ex03/include/HumanA.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanA.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/14 21:18:08 by lucperei          #+#    #+#             */
+/*   Updated: 2024/04/14 21:18:11 by lucperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef HUMANA_HPP
 #define HUMANA_HPP
 
@@ -7,12 +19,12 @@
 
 class	HumanA
 {
-private:
-	std::string _name;
-	Weapon _weapon;
-public:
-	HumanA(const std::string& humanName, const Weapon& initialWeapon);
-	void	attack(void);
+	private:
+		std::string _name;
+		Weapon &_weapon;
+	public:
+		HumanA(std::string name, Weapon& weapon);
+		void	attack(void);
 };
 
 #endif
