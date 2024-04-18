d1ddb7d60fb567b5b0d38237ecf190cf7478c037
Luciana Pereira <rm92014@fiap.com.br>
2024-04-18 14:34:29 -0300

    Update Fixed.cpp

--- cpp02/ex03/src/Fixed.cpp
+++ cpp02/ex03/src/Fixed.cpp
@@ -10,10 +10,48 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+Construtor Padrão (Fixed::Fixed(void)): Inicializa um objeto Fixed com o valor _value igual a 0.
+
+Construtor de Cópia (Fixed::Fixed(const Fixed &rhs)): Cria um novo objeto Fixed a partir de outro objeto
+Fixed fornecido como argumento.
+
+Construtor com Inteiro (Fixed::Fixed(const int intValue)): Inicializa um objeto Fixed com o valor 
+_value igual ao inteiro fornecido, convertido para fixo.
+
+Construtor com Ponto Flutuante (Fixed::Fixed(const float floatValue)): Inicializa um objeto Fixed 
+com o valor _value igual ao ponto flutuante fornecido, convertido para fixo.
+
+Destrutor (Fixed::~Fixed()): Destrói um objeto Fixed.
+
+Operador de Atribuição (Fixed& Fixed::operator=(const Fixed& other)): Sobrecarrega o operador de 
+atribuição para permitir a cópia dos membros de um objeto Fixed para outro.
+
+Operadores de Comparação (bool Fixed::operator>(const Fixed& other), bool 
+Fixed::operator<(const Fixed& other), etc.): Sobrecarrega os operadores de comparação para comparar dois objetos Fixed.
+
+Operadores Aritméticos (Fixed Fixed::operator+(const Fixed& other), Fixed Fixed::operator-(const Fixed& other), etc.): Sobrecarrega os operadores aritméticos para realizar operações matemáticas entre dois objetos Fixed.
+
+Operadores de Incremento e Decremento (Fixed& Fixed::operator++(), Fixed Fixed::operator++(int), etc.): Sobrecarrega os operadores de incremento e decremento para modificar o valor de um objeto Fixed.
+
+Método Estático min: Retorna o menor valor entre dois objetos Fixed.
+
+Método Estático max: Retorna o maior valor entre dois objetos Fixed.
+
+Método getRawBits: Retorna o valor armazenado no objeto Fixed.
+
+Método setRawBits: Define o valor armazenado no objeto Fixed.
+
+Método toFloat: Converte o valor armazenado no objeto Fixed para um ponto flutuante.
+
+Método toInt: Converte o valor armazenado no objeto Fixed para um inteiro.
+
+Sobrecarga do Operador de Saída (std::ostream& operator<<(std::ostream& out, const Fixed& fixed)): Permite a saída de um objeto Fixed para um fluxo de saída (como std::cout), convertendo o valor para ponto flutuante.
+
+Esse código implementa uma classe Fixed que representa um número de ponto fixo e sobrecarrega diversos operadores para permitir operações matemáticas e comparações entre objetos Fixed.
+*/
 #include "../include/Fixed.hpp"
-
-const int	Fixed::_fractionalBits = 8;
-
+ /
 Fixed::Fixed(void) : _value(0) {}
 
 Fixed::Fixed(const Fixed &rhs)
