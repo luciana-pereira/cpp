e4d28e0f9a6a91d0ce1bf7ee97dd4af17f6a2f47
Luciana Pereira <rm92014@fiap.com.br>
2024-04-18 14:21:36 -0300

    Update Point.cpp

--- cpp02/ex03/src/Point.cpp
+++ cpp02/ex03/src/Point.cpp
@@ -12,17 +12,42 @@
 
 #include "../include/Point.hpp"
 
+/*
+Essas funções e métodos definem a classe Point, que representa um ponto no plano 2D, e 
+fornecem funcionalidades básicas para criar, copiar e acessar os membros de um objeto Point.
+*/
+
+
+/*
+Construtor Padrão (Point::Point(void)): Inicializa um objeto Point com coordenadas x e y iguais a 0.
+*/
 Point::Point(void) : _x(0), _y(0) {}
 
+/*
+Construtor com Parâmetros (Point::Point(const float x, const float y)): Inicializa um objeto Point 
+com as coordenadas x e y fornecidas como parâmetros.
+*/
 Point::Point(const float x, const float y) : _x(x), _y(y) {}
 
+/*
+Construtor de Cópia (Point::Point(const Point &src)): Cria um novo objeto Point a partir de outro objeto 
+Point fornecido como argumento.
+*/
 Point::Point(const Point &src)
 {
 	*this = src;
 }
 
+/*
+Destrutor (Point::~Point()): É responsável por destruir um objeto Point. No caso, como não há nada específico a ser 
+liberado ou desalocado, ele está vazio.
+*/
 Point::~Point() {}
 
+/*
+Operador de Atribuição (Point &Point::operator=(const Point &rhs)): Sobrecarrega o operador de atribuição para permitir a 
+cópia dos membros de um objeto Point para outro objeto Point.
+*/
 Point &Point::operator=(const Point &rhs)
 {
 	const_cast<Fixed&>(this->_x) = rhs._x;
@@ -30,11 +55,12 @@ Point &Point::operator=(const Point &rhs)
 	return (*this);
 }
 
+//Método getX: Retorna a coordenada x do objeto Point.
 Fixed Point::getX(void) const
 {
 	return (this->_x);
 }
-
+ // Método getY: Retorna a coordenada y do objeto Point.
 Fixed Point::getY(void) const
 {
 	return (this->_y);
