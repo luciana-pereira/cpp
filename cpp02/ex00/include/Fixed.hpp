7bb85ea4584a65f477eea63340b93d3d2a12b544
Luciana Pereira <rm92014@fiap.com.br>
2024-04-17 21:09:03 -0300

    Update Fixed.hpp

--- cpp02/ex00/include/Fixed.hpp
+++ cpp02/ex00/include/Fixed.hpp
@@ -12,8 +12,11 @@ public:
 	Fixed(void);
 	Fixed(const Fixed& other);
 	~Fixed(void);
+	// Declaração das funções membro que retorna o valor bruto de bits do número fixo.
 	int	getRawBits(void) const;
 	void	setRawBits(int const raw);
+	// Operador de atribuição, sobrecarga do operador de atribuição da classe Fixed, 
+	// que indica que estamos sobrecarregando o operador de atribuição (=) na classe Fixed.
 	Fixed& operator=(const Fixed &other);
 };
 
