efc87734c627876fe3223c46773266eddb017cfa
Luciana Pereira <rm92014@fiap.com.br>
2024-04-18 00:33:37 -0300

    Update main.cpp

--- cpp02/ex01/src/main.cpp
+++ cpp02/ex01/src/main.cpp
@@ -12,24 +12,30 @@
 
 #include "../include/Fixed.hpp"
 
+// Criação de objetos da classe Fixed e a realização de operações com eles. 
 int	main(void)
 {
+	// Cria um objeto Fixed chamado a usando o construtor padrão, que inicializa o valor como zero.
 	Fixed	a;
+	// Cria um objeto Fixed constante b com o valor 10 usando o construtor que aceita um inteiro como parâmetro.
 	Fixed const b(10);
+	// Cria um objeto Fixed constante c com o valor 42.42 usando o construtor que aceita um float como parâmetro.
 	Fixed const c(42.42f);
+	// Cria um objeto Fixed constante d com base no objeto b, utilizando o construtor de cópia.
 	Fixed const d(b);
-
-	a = Fixed(1234.4321f);
 	
+	// Atribui o valor 1234.4321 ao objeto a, convertendo-o para ponto flutuante.
+	a = Fixed(1234.4321f);
+	// Imprime os valores dos objetos a, b, c e d 
 	std::cout << "a is " << a << std::endl;
 	std::cout << "b is " << b << std::endl;
 	std::cout << "c is " << c << std::endl;
 	std::cout << "d is " << d << std::endl;
-	
+	// Imprime os valores dos objetos a, b, c e d como inteiros na saída padrão, usando a função toInt() da classe Fixed.
 	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
-	
+	//  o código cria objetos Fixed com diferentes valores, os imprime na tela e verifica se eles podem ser convertidos corretamente para inteiros.
 	return (0);
 }
