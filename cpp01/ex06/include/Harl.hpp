303a20d8f7dc89f2fd5db00723f0ab7888ca22df
Luciana Pereira <rm92014@fiap.com.br>
2024-04-23 22:00:39 -0300

    Update Harl.hpp

--- cpp01/ex06/include/Harl.hpp
+++ cpp01/ex06/include/Harl.hpp
@@ -1,15 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/15 01:20:23 by lucperei          #+#    #+#             */
+/*   Updated: 2024/04/15 01:40:40 by lucperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef HARL_HPP
 #define HARL_HPP
 
 #include <iostream>
 #include <string>
-
-enum Level {
-	DEBUG,
-	INFO,
-	WARNING,
-	ERROR
-};
 
 class	Harl
 {
@@ -17,6 +22,12 @@ class	Harl
 		Harl(void);
 		~Harl(void);
 		void	complain(std::string level);
+		enum Level {
+			DEBUG,
+			INFO,
+			ERROR,
+			WARNING
+		};
 	private:
 		void	debug(void);
 		void	info(void);
