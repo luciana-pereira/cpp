d4545317ec9dd2b727ff7cab3b24d09cc85c8ec1
Luciana Pereira <rm92014@fiap.com.br>
2024-04-23 21:59:56 -0300

    Update main.cpp

--- cpp01/ex06/src/main.cpp
+++ cpp01/ex06/src/main.cpp
@@ -1,58 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/15 01:30:51 by lucperei          #+#    #+#             */
+/*   Updated: 2024/04/15 01:33:31 by lucperei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/Harl.hpp"
 #include <cctype>
 
 int	main(int argc, char *argv[])
 {
-	Harl		harl;
-	int	options;
+	Harl filter;
+	bool isInvalidArg = true;
 
 	if (argc != 2)
 	{
 		std::cerr << "Usage: " << argv[0] << " <log_level>" << std::endl;
 		return (-1);
 	}
+
 	std::string validLevels[4] = {
 		"DEBUG",
 		"INFO",
 		"WARNING",
 		"ERROR"
 	};
-	options = -1;
+
 	std::string logLevel(argv[1]);
+
 	for (size_t index = 0; index < logLevel.length(); ++index)
 	{
 		logLevel[index] = std::toupper(logLevel[index]);
 	}
+
 	for (int i = 0; i < 4; i++)
 	{
 		if (std::string(logLevel) == validLevels[i])
-			options = i;
-    }
-	// harl.complain(logLevel);
-	Harl	filter;
-	switch (options)
-	{
-		case DEBUG:
-			filter.complain("DEBUG");
-			filter.complain("INFO");
-			filter.complain("WARNING");
-			filter.complain("ERROR");
-			break ;
-		case INFO:
-			filter.complain("INFO");
-			filter.complain("WARNING");
-			filter.complain("ERROR");
-			break ;
-		case WARNING:
-			filter.complain("WARNING");
-			filter.complain("ERROR");
-			break ;
-		case ERROR:
-			filter.complain("ERROR");
-			break ;
-		default:
-			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
-			break ;
+		{
+			isInvalidArg = false;
+			filter.complain(validLevels[i]);
+			break;
+		}
 	}
-	return (0);
+
+	if (isInvalidArg)
+		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+
+	return 0;
 }
