a0782d7a48854e4e916981ad4725fa53691e2b5a
Luciana Pereira <rm92014@fiap.com.br>
2024-05-10 15:19:31 -0300

    Update main.cpp

--- cpp04/ex01/src/main.cpp
+++ cpp04/ex01/src/main.cpp
@@ -26,15 +26,15 @@ int	main(void)
 	const Animal* dog = new Dog();
 	const Animal* cat = new Cat();
 	std::cout << std::endl;
-    std::cout << "Animal type: " << animal->getType() << std::endl;
-    std::cout << "Dog type: " << dog->getType() << std::endl;
-    std::cout << "Cat type: " << cat->getType() << std::endl;
+    	std::cout << "Animal type: " << animal->getType() << std::endl;
+    	std::cout << "Dog type: " << dog->getType() << std::endl;
+    	std::cout << "Cat type: " << cat->getType() << std::endl;
 	std::cout << std::endl;
 	animal->makeSound();
 	dog->makeSound();
 	cat->makeSound();
 	
-    std::cout << "============================= End of Type and Sound Test =====================" << std::endl;
+    	std::cout << "============================= End of Type and Sound Test =====================" << std::endl;
 	
 	delete animal;
 	animal = NULL;
@@ -48,13 +48,13 @@ int	main(void)
 	const WrongAnimal* wrongAnimal = new WrongAnimal();
 	const WrongAnimal* wrongCat = new WrongCat();
 	std::cout << std::endl;
-    std::cout << "WrongCat type: " << wrongCat->getType() << std::endl;
-    std::cout << "WrongAnimal type: " << wrongAnimal->getType() << std::endl;
+    	std::cout << "WrongCat type: " << wrongCat->getType() << std::endl;
+    	std::cout << "WrongAnimal type: " << wrongAnimal->getType() << std::endl;
 	std::cout << std::endl;
 	wrongCat->makeSound(); 
 	wrongAnimal->makeSound();
 	
-    std::cout << "============================= End of WrongAnimal Test ========================" << std::endl;
+    	std::cout << "============================= End of WrongAnimal Test ========================" << std::endl;
 	
 	delete wrongAnimal;
 	wrongAnimal = NULL;
@@ -63,7 +63,7 @@ int	main(void)
 
 	std::cout << "============================= Memory Leak Check ===============================" << std::endl;
 	// Additional tests to check for memory leaks
-    {
+   	{
         const int animalCount = 6;
         Animal* animalArray[animalCount];
 
@@ -81,7 +81,7 @@ int	main(void)
         // Delete the array of Animal objects
         for (int i = 0; i < animalCount; ++i)
             delete animalArray[i];
-    }
+    	}
 	
 	return (0);
 }
