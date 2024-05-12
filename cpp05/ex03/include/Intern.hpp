6449fc7d7287cc3f9e2f4a3bc611ccc39bd5374a
Luciana Pereira <rm92014@fiap.com.br>
2024-05-12 18:29:00 -0300

    Update Intern.hpp

--- cpp05/ex03/include/Intern.hpp
+++ cpp05/ex03/include/Intern.hpp
@@ -6,25 +6,32 @@
 /*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 19:41:42 by lucperei          #+#    #+#             */
-/*   Updated: 2024/02/17 21:33:40 by lucperei         ###   ########.fr       */
+/*   Updated: 2024/05/12 18:42:17 by lucperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INTERN_HPP
 #define INTERN_HPP
 
-#include "AForm.hpp"
-
-class AForm;
+#include <string>
+#include <iostream>
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 
 class	Intern
 {
 public:
-    Intern(void);
-    Intern(const Intern &obj);
-    ~Intern(void);
-    Intern &operator=(const Intern &obj);
-    AForm* makeForm(const std::string& request, const std::string& target);
+	Intern(void);
+	Intern(Intern &obj);
+	virtual ~Intern(void);
+	Intern &operator=(Intern &obj);
+	AForm* makeForm(const std::string& formName, const std::string& target);
+private:
+	struct FormCreationInfo {
+		const char* formName;
+		AForm* (*createForm)(const std::string& target);
+	};
 };
 
 #endif
