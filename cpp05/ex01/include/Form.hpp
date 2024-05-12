34f17a93381bbf7dcf466c9446f288e542419d41
Luciana Pereira <rm92014@fiap.com.br>
2024-05-12 18:14:11 -0300

    Update Form.hpp

--- cpp05/ex01/include/Form.hpp
+++ cpp05/ex01/include/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 03:50:49 by lucperei          #+#    #+#             */
-/*   Updated: 2024/02/17 03:55:41 by lucperei         ###   ########.fr       */
+/*   Updated: 2024/05/11 19:49:45 by lucperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,23 +25,24 @@ private:
 	const int			_gradeToSign;
 	const int			_gradeToExecute;
 public:
-	class GradeTooHighException : public std::exception {
+	class	GradeTooHighException : public std::exception {
 	public:
 		const char* what() const throw();
 	};
-	class GradeTooLowException : public std::exception {
+	class	GradeTooLowException : public std::exception {
 	public:
 		const char* what() const throw();
 	};
+	Form(void);
+	Form(Form& form);
 	Form(const std::string& name, int gradeToSign, int gradeToExecute);
-	Form(const Form& form);
-	Form& operator=(const Form& form);
+	Form& operator=(Form& form);
 	~Form(void);
-	const std::string& getName(void) const;
-	bool	isSigned(void) const;
+	const	std::string& getName(void) const;
+	bool	getIsSigned(void) const;
 	int	getGradeToSign(void) const;
 	int	getGradeToExecute(void) const;
-	void	beSigned(const Bureaucrat& bureaucrat);
+	void	beSigned(Bureaucrat& bureaucrat);
 };
 
 std::ostream&	operator<<(std::ostream& os, const Form& form);
