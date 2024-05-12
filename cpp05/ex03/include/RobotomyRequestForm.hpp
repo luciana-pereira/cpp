ec83a02a6f9cd5d4d8e289b7d96d7e24b699b45b
Luciana Pereira <rm92014@fiap.com.br>
2024-05-12 18:30:02 -0300

    Update RobotomyRequestForm.hpp

--- cpp05/ex03/include/RobotomyRequestForm.hpp
+++ cpp05/ex03/include/RobotomyRequestForm.hpp
@@ -6,7 +6,7 @@
 /*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 05:48:17 by lucperei          #+#    #+#             */
-/*   Updated: 2024/02/17 06:10:07 by lucperei         ###   ########.fr       */
+/*   Updated: 2024/05/12 16:48:22 by lucperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@
 class	RobotomyRequestForm : public AForm
 {
 private:
-	const std::string _target;
+	std::string _target;
 public:
 	RobotomyRequestForm(void);
-	RobotomyRequestForm(const std::string& target);
-	RobotomyRequestForm(const RobotomyRequestForm& form);
-	RobotomyRequestForm& operator=(const RobotomyRequestForm& form);
-	~RobotomyRequestForm(void);
-	bool	execute(const Bureaucrat& executor) const;
+	RobotomyRequestForm(std::string target);
+	RobotomyRequestForm(RobotomyRequestForm& form);
+	RobotomyRequestForm& operator=(RobotomyRequestForm& form);
+	virtual ~RobotomyRequestForm(void);
+	virtual bool	execute(Bureaucrat const& executor) const;
 };
 
 #endif
