88c03466741cb53139f71cd4a821be9f5c7ce723
Luciana Pereira <rm92014@fiap.com.br>
2024-05-12 18:34:26 -0300

    Update main.cpp

--- cpp05/ex03/src/main.cpp
+++ cpp05/ex03/src/main.cpp
@@ -6,68 +6,89 @@
 /*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 14:11:43 by lucperei          #+#    #+#             */
-/*   Updated: 2024/02/17 21:36:10 by lucperei         ###   ########.fr       */
+/*   Updated: 2024/05/12 17:36:59 by lucperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/Bureaucrat.hpp"
 #include "../include/AForm.hpp"
 #include "../include/ShrubberyCreationForm.hpp"
 #include "../include/RobotomyRequestForm.hpp"
 #include "../include/PresidentialPardonForm.hpp"
 #include "../include/Intern.hpp"
 
-void    bureaucrat_test() {
-    try {
-        Bureaucrat highGradeBureaucrat("John", 1);
-        std::cout << highGradeBureaucrat << std::endl;
+void	bureaucrat_test(void)
+{
+	try {
+		std::cout << "\n============ Start of testing =============\n" << std::endl;
 
-        Bureaucrat lowGradeBureaucrat("Jane", 150);
-        std::cout << lowGradeBureaucrat << std::endl;
+		Bureaucrat highGradeBureaucrat("John", 1);
+		std::cout << highGradeBureaucrat << std::endl;
 
-        // Test ShrubberyCreationForm
-        ShrubberyCreationForm shrubberyForm("Home");
-        std::cout << shrubberyForm << std::endl;
+		Bureaucrat lowGradeBureaucrat("Jane", 150);
+		std::cout << lowGradeBureaucrat << std::endl;
 
-        shrubberyForm.beSigned(highGradeBureaucrat);
-        highGradeBureaucrat.executeForm(shrubberyForm);
+		std::cout << "\n======= Test ShrubberyCreationForm ========\n" << std::endl;
+		ShrubberyCreationForm shrubberyForm("Home");
 
-        // Test RobotomyRequestForm
-        RobotomyRequestForm robotomyForm("Target");
-        std::cout << robotomyForm << std::endl;
+		shrubberyForm.beSigned(highGradeBureaucrat);
+		highGradeBureaucrat.executeForm(shrubberyForm);
+		
+		std::cout << shrubberyForm << std::endl;
 
-        robotomyForm.beSigned(lowGradeBureaucrat);
-        lowGradeBureaucrat.executeForm(robotomyForm);
+		std::cout << "\n======== Test RobotomyRequestForm =========\n" << std::endl;
+		RobotomyRequestForm robotomyForm("Target");
 
-        // Test PresidentialPardonForm
-        PresidentialPardonForm pardonForm("Criminal");
-        std::cout << pardonForm << std::endl;
+		robotomyForm.beSigned(highGradeBureaucrat);
+		highGradeBureaucrat.executeForm(robotomyForm);
+		
+		std::cout << robotomyForm << std::endl;
 
-        pardonForm.beSigned(highGradeBureaucrat);
-        highGradeBureaucrat.executeForm(pardonForm);
-    } catch (const std::exception& e) {
-        std::cerr << "Exception caught: " << e.what() << std::endl;
-    }
+		std::cout << "\n****RobotomyRequestForm not executed, grade is too low!\n" << std::endl;
+		RobotomyRequestForm robotomyForm1("Target");
+
+		robotomyForm.beSigned(lowGradeBureaucrat);
+		lowGradeBureaucrat.executeForm(robotomyForm1);
+		
+		std::cout << robotomyForm1 << std::endl;
+
+		std::cout << "\n======= Test PresidentialPardonForm =======\n" << std::endl;
+		PresidentialPardonForm pardonForm("Criminal");
+
+		pardonForm.beSigned(highGradeBureaucrat);
+		highGradeBureaucrat.executeForm(pardonForm);
+		
+		std::cout << pardonForm << std::endl;
+
+	} catch (const std::exception& e) {
+		std::cerr << "Exception caught: " << e.what() << std::endl;
+	}
 }
 
-void    intern_test() {
-    try {
-        // Test Intern class
-        Intern someRandomIntern;
-        AForm* rrf = someRandomIntern.makeForm("robotomy request", "Bender");
+void	intern_test(void)
+{
+	try {
+		std::cout << "============ Test Intern class ============\n" << std::endl;
 
-        Bureaucrat highGradeBureaucrat("John", 1);
-        highGradeBureaucrat.checkSignedForm(*rrf);
-        highGradeBureaucrat.executeForm(*rrf);
-        delete rrf;
-    } catch (const std::exception& e) {
-        std::cerr << "Exception caught: " << e.what() << std::endl;
-    }
+		Intern someRandomIntern;
+		AForm* rrf = someRandomIntern.makeForm("robotomy request", "Bender");
+
+		Bureaucrat highGradeBureaucrat("John", 1);
+		highGradeBureaucrat.checkSignedForm(*rrf);
+		highGradeBureaucrat.executeForm(*rrf);
+		
+		std::cout << *rrf << std::endl;
+		
+		delete rrf;
+		std::cout << std::endl;
+	} catch (const std::exception& e) {
+		std::cerr << "Exception caught: " << e.what() << std::endl;
+	}
 }
 
-int main(void) {
-    bureaucrat_test();
-    std::cout << std::endl;
-    intern_test();
-    return 0;
+int main(void)
+{
+	bureaucrat_test();
+	std::cout << std::endl;
+	intern_test();
+	return (0);
 }
