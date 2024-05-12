98bb1295e9236a90c54169990f7dbb0f47d11c9f
Luciana Pereira <rm92014@fiap.com.br>
2024-05-12 14:08:16 -0300

    Update ShrubberyCreationForm.cpp

--- cpp05/ex03/src/ShrubberyCreationForm.cpp
+++ cpp05/ex03/src/ShrubberyCreationForm.cpp
@@ -12,23 +12,57 @@
 
 #include "../include/ShrubberyCreationForm.hpp"
 
+/* 
+Construtor padrão, que inicializa um objeto ShrubberyCreationForm com um valor padrão 
+para o atributo _target e chama o construtor da classe base AForm com os valores específicos 
+para o formulário de criação.
+*/
 ShrubberyCreationForm::ShrubberyCreationForm(void)
 	: AForm("ShrubberyCreationForm", 145, 137), _target("default") {}
 
+/*
+Construtor sobrecarregado que permite inicializar um objeto ShrubberyCreationForm com um valor 
+específico para o atributo _target. Ele chama o construtor da classe base AForm com os valores 
+específicos para o formulário de criação.
+*/
 ShrubberyCreationForm::ShrubberyCreationForm(const std::string& target)
 	: AForm("ShrubberyCreationForm", 145, 137), _target(target) {}
 
+/*
+Construtor de cópia da classe ShrubberyCreationForm. Ele cria uma cópia de outro objeto 
+ShrubberyCreationForm, copiando o valor do atributo _target e chamando o construtor de cópia 
+da classe base AForm.
+*/
 ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& form)
 	: AForm("ShrubberyCreationForm", 145, 137), _target(form._target) {}
 
+/*
+Operador de atribuição da classe ShrubberyCreationForm. Ele permite atribuir o valor de outro 
+objeto ShrubberyCreationForm a este objeto, copiando o valor do atributo _target e chamando o 
+operador de atribuição da classe base AForm.
+*/
 ShrubberyCreationForm& ShrubberyCreationForm::operator=(const ShrubberyCreationForm& form)
 {
 	AForm::operator=(form);
 	return (*this);
 }
 
+/*
+Destrutor da classe ShrubberyCreationForm. Ele é chamado quando um objeto ShrubberyCreationForm é 
+destruído e libera quaisquer recursos alocados dinamicamente, mas neste caso, parece não haver 
+alocação dinâmica.
+*/
 ShrubberyCreationForm::~ShrubberyCreationForm(void) {}
 
+/*
+Método execute da classe ShrubberyCreationForm, que implementa a execução do formulário de criação. 
+Ele recebe um objeto Bureaucrat como parâmetro, que representa quem está executando o formulário. 
+O método verifica se o executor tem a classificação necessária para executar o formulário. Se o 
+executor tiver uma classificação adequada, o método abre um arquivo e escreve uma representação de 
+um arbusto em ASCII. Se a classificação do executor for muito baixa, uma exceção GradeTooLowException 
+é lançada e tratada dentro do método. O método retorna true se a execução for bem-sucedida e false 
+se o executor tiver uma classificação/nota muito baixa.
+*/
 bool	ShrubberyCreationForm::execute(const Bureaucrat& executor) const
 {
 	try {
