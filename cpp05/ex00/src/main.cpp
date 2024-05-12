ada0d1aaac7322148e99c3e2de7ab24b0f8a944c
Luciana Pereira <rm92014@fiap.com.br>
2024-05-12 18:10:44 -0300

    Update main.cpp

--- cpp05/ex00/src/main.cpp
+++ cpp05/ex00/src/main.cpp
@@ -6,20 +6,24 @@
 /*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 14:11:43 by lucperei          #+#    #+#             */
-/*   Updated: 2024/02/16 05:43:51 by lucperei         ###   ########.fr       */
+/*   Updated: 2024/05/12 17:45:19 by lucperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Bureaucrat.hpp"
 
-int main(void)
+int	main(void)
 {
 	try {
+		std::cout << " ========= Init Test ========= " << std::endl;
+		std::cout << "\n ====== Creat Bureaucrat ===== \n" << std::endl;
 		Bureaucrat highGradeBureaucrat("John", 1);
 		std::cout << highGradeBureaucrat << std::endl;
 
 		Bureaucrat lowGradeBureaucrat("Jane", 150);
 		std::cout << lowGradeBureaucrat << std::endl;
+				
+		std::cout << "\n=== Higher or Lower Grade ====\n" << std::endl;
 
 		highGradeBureaucrat.incrementGrade();
 		std::cout << highGradeBureaucrat << std::endl;
@@ -27,11 +31,12 @@ int main(void)
 		lowGradeBureaucrat.decrementGrade();
 		std::cout << lowGradeBureaucrat << std::endl;
 
-		// Trying to create a bureaucrat with an invalid grade
+		std::cout << "\n======= Invalid Grade ========\n" << std::endl;
+
 		Bureaucrat invalidGradeBureaucrat("Invalid", 151);
+		std::cout << std::endl;
 	} catch (const std::exception& e) {
 		std::cerr << "Exception caught: " << e.what() << std::endl;
 	}
 	return (0);
 }
-
