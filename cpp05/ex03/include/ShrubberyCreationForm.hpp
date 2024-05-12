732620d4e2d3314f287f913eadd0b36de56642e9
Luciana Pereira <rm92014@fiap.com.br>
2024-05-12 18:30:39 -0300

    Update ShrubberyCreationForm.hpp

--- cpp05/ex03/include/ShrubberyCreationForm.hpp
+++ cpp05/ex03/include/ShrubberyCreationForm.hpp
@@ -6,7 +6,7 @@
 /*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 06:33:12 by lucperei          #+#    #+#             */
-/*   Updated: 2024/02/17 06:34:49 by lucperei         ###   ########.fr       */
+/*   Updated: 2024/05/12 17:38:10 by lucperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@
 class	ShrubberyCreationForm : public AForm
 {
 private:
-	const std::string _target;
+	std::string _target;
 public:
 	ShrubberyCreationForm(void);
-	ShrubberyCreationForm(const std::string& target);
-	ShrubberyCreationForm(const ShrubberyCreationForm& form);
-	ShrubberyCreationForm& operator=(const ShrubberyCreationForm& form);
-	~ShrubberyCreationForm(void);
-	bool	execute(const Bureaucrat& executor) const;
+	ShrubberyCreationForm(std::string target);
+	ShrubberyCreationForm(ShrubberyCreationForm& form);
+	ShrubberyCreationForm& operator=(ShrubberyCreationForm& form);
+	virtual ~ShrubberyCreationForm(void);
+	virtual bool	execute(Bureaucrat const& executor) const;
 };
 
 #endif
