8ad6321181b756472b657ac63691602f0fcad3eb
Luciana Pereira <rm92014@fiap.com.br>
2024-05-12 18:23:16 -0300

    Update PresidentialPardonForm.cpp

--- cpp05/ex02/src/PresidentialPardonForm.cpp
+++ cpp05/ex02/src/PresidentialPardonForm.cpp
@@ -6,22 +6,22 @@
 /*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 05:14:28 by lucperei          #+#    #+#             */
-/*   Updated: 2024/02/17 07:53:35 by lucperei         ###   ########.fr       */
+/*   Updated: 2024/05/12 01:28:32 by lucperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/PresidentialPardonForm.hpp"
 
 PresidentialPardonForm::PresidentialPardonForm(void)
-	: AForm("PresidentialPardonForm", 25, 5), _target("default") {}
+	: AForm("PresidentialPardonForm", 25, 5), _target("Unnamed") {}
 
 PresidentialPardonForm::PresidentialPardonForm(const std::string& target)
 	: AForm("PresidentialPardonForm", 25, 5), _target(target) {}
 
-PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm& form)
+PresidentialPardonForm::PresidentialPardonForm(PresidentialPardonForm& form)
 	: AForm("PresidentialPardonForm", 25, 5), _target(form._target) {}
 
-PresidentialPardonForm& PresidentialPardonForm::operator=(const PresidentialPardonForm& form)
+PresidentialPardonForm& PresidentialPardonForm::operator=(PresidentialPardonForm& form)
 {
 	AForm::operator=(form);
 	return (*this);
