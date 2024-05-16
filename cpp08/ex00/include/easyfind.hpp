a42ec6a7455f76d8cc61528772cbf0632d96b61a
Luciana Pereira <rm92014@fiap.com.br>
2024-05-16 17:14:44 -0300

    Update easyfind.hpp

--- cpp08/ex00/include/easyfind.hpp
+++ cpp08/ex00/include/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 03:54:39 by lucperei          #+#    #+#             */
-/*   Updated: 2024/03/05 04:02:16 by lucperei         ###   ########.fr       */
+/*   Updated: 2024/05/15 05:43:03 by lucperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,29 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <list>
 
+// Iterador 
 template <typename T>
 typename T::iterator easyfind(T &container, int value) {
-	typename T::iterator it = std::find(container.begin(), container.end(), value);
+	typename T::iterator it = std::find(container.begin(), container.end(), value); // Procura pelo valor no container
 
+	if (it == container.end()) { // // Se o valor não for encontrado
+		throw std::runtime_error("Value not found in container"); ///Lança uma execeção
+	}
+
+	return (it); // Retorna o iterador para o valor encontrado
+}
+
+template <typename T>
+typename T::const_iterator easyfind(const T &container, int value) {
+	const typename T::const_iterator it = std::find(container.begin(), container.end(), value);
+	
 	if (it == container.end()) {
 		throw std::runtime_error("Value not found in container");
 	}
 
-	return it;
+	return (it);
 }
 
 #endif
