da845dba300ec4ee5ff1095178e5580ca8340fbb
Luciana Pereira <rm92014@fiap.com.br>
2024-05-16 16:36:52 -0300

    Update Span.hpp

--- cpp08/ex01/include/Span.hpp
+++ cpp08/ex01/include/Span.hpp
@@ -6,30 +6,60 @@
 /*   By: lucperei <lucperei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 04:01:09 by lucperei          #+#    #+#             */
-/*   Updated: 2024/03/05 04:11:04 by lucperei         ###   ########.fr       */
+/*   Updated: 2024/05/16 06:06:39 by lucperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SPAN_HPP
 #define SPAN_HPP
 
-#include <iostream>
 #include <algorithm>
+#include <iostream>
+#include <cstdlib>
 #include <limits>
-#include <list>
+#include <vector>
+#include <ctime>
 
-class	Span {
+class Span {
 public:
+	// Construtor padrão
+	Span(void);
+	// Construtor com limite de tamanho N
 	Span(unsigned int N);
-	~Span();
+	// Construtor de cópia
+	Span(Span const &other);
+	// Destrutor
+	~Span(void);
+	// Sobrecarga do operador de atribuição
+	Span &operator=(Span const &other);
 
-	void	addNumber(int number);
-	int		shortestSpan();
-	int		longestSpan();
-	void	addNumbers(std::list<int>::iterator begin, std::list<int>::iterator end);
+	// Adiciona um número à lista de números
+	void		addNumber(int number);
+	// Calcula o menor intervalo entre os números
+	long int	shortestSpan(void);
+	// Calcula o maior intervalo entre os números
+	long int	longestSpan(void);
+	// Imprime todos os números armazenados
+	void		printNumbers(void) const;
+
+	// Adiciona um intervalo de números à lista de números
+	template<typename T>
+	void		addRangeOfNumbers(T a, T b) {
+		// Verifica se há espaço suficiente para adicionar todos os números do intervalo
+		if (std::distance(a, b) > this->_N - (long long)this->_numbers.size()) {
+			throw std::exception();
+		} 
+		
+		// Adiciona os números do intervalo à lista de números
+		while (a != b) {
+			this->_numbers.push_back(*a);
+			a++;
+		}
+	}
+
 private:
-    unsigned int _N;
-    std::list<int> _numbers;
+	unsigned int _N; // Limite de tamanho
+	std::vector<int> _numbers; // Lista de números armazenados
 };
 
 #endif
